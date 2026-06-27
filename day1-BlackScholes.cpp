ece65f1b26bb41ce462e97fcd808d8eb0ea7c6e8
driftalpha <senneldev@gmail.com>
2026-06-27 19:11:04 +0200

    Day one

--- day1-BlackScholes.cpp
+++ day1-BlackScholes.cpp
@@ -7,7 +7,7 @@ template <typename T>
 auto normalCDF(T x) -> std::common_type_t <double , T>
 {
     using commonType = std::common_type_t<double, T>;
-    return static_cast<commonType>(0.5) * std::erfc(-x / std::sqrt(2.0));
+    return static_cast<commonType>(0.5) * (static_cast<commonType>(1) + std::erf(x / std::sqrt(2.0)));
 }
 
 
