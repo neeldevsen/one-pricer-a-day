c6ecbb6d9604a4cf309f1db6ef884d29ba4bb66c
Neel Dev Sen <164937276+neeldevsen@users.noreply.github.com>
2026-06-27 20:45:44 +0200

    Clean up whitespace in Black-Scholes implementation
    Removed unnecessary blank lines for improved readability.

--- day1-BlackScholes.cpp
+++ day1-BlackScholes.cpp
@@ -2,14 +2,12 @@
 #include <cmath>
 #include <type_traits>
 
-
 template <typename T>
 auto normalCDF(T x) -> std::common_type_t <double , T>
 {
     using commonType = std::common_type_t<double, T>;
     return static_cast<commonType>(0.5) * (static_cast<commonType>(1) + std::erf(x / std::sqrt(2.0)));
 }
-
 
 template <typename SType, typename KType, typename rType, typename sigmaType, typename tType>
 auto blackScholesCall(SType S, KType K, rType r, sigmaType sigma, tType t) -> std::common_type_t<double, SType, KType, rType, sigmaType, tType>
@@ -21,15 +19,12 @@ auto blackScholesCall(SType S, KType K, rType r, sigmaType sigma, tType t) -> st
     auto sigma_new {static_cast<commonType>(sigma)};
     auto t_new {static_cast<commonType>(t)};
 
-
     auto d1 {(std::log(S_new / K_new) + (r_new + 0.5 * sigma_new * sigma_new) * t_new) / (sigma_new * std::sqrt(t_new))};
     auto d2 {d1 - sigma_new * std::sqrt(t_new)};
-
 
     auto C {S_new * normalCDF(d1) - K_new * std::exp(-r_new * t_new) * normalCDF(d2)};
     return C;
 }
-
 
 template <typename SType, typename KType, typename rType, typename sigmaType, typename tType>
 auto blackScholesPut(SType S, KType K, rType r, sigmaType sigma, tType t) -> std::common_type_t<double, SType, KType, rType, sigmaType, tType>
@@ -41,15 +36,12 @@ auto blackScholesPut(SType S, KType K, rType r, sigmaType sigma, tType t) -> std
     auto sigma_new {static_cast<commonType>(sigma)};
     auto t_new {static_cast<commonType>(t)};
 
-
     auto d1 {(std::log(S_new / K_new) + (r_new + 0.5 * sigma_new * sigma_new) * t_new) / (sigma_new * std::sqrt(t_new))};
     auto d2 {d1 - sigma_new * std::sqrt(t_new)};
-
 
     auto P {K_new * std::exp(-r_new * t_new) * normalCDF(-d2) - S_new * normalCDF(-d1)};
     return P;
 }
-
 
 int main()
 {
